0c93feade58fca676f788a36f93049cb34540fd8
slama-pakhrin <salonlama47@gmail.com>
2024-02-08 16:25:22 -0500

    Initial Commit

---
+++ AssignmentOne/ViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ViewController.swift
+//  AssignmentOne
+//
+//  Created by Arii on 2024-02-08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let courseName : [String] = ["CPA", "BSD"]
+    let experienceYears : [String] = ["Freshy", "Around Two years", "More than 5 years"]
+    let levelAs : [String] = ["Level1", "Level2", "Level3", "Level4", "Level5"]
+    
+    var courseDisplayName : String?
+    var presentLevel = "Level1"
+    var experienceName : String?
+    
+    @IBOutlet weak var course: UISegmentedControl!
+    @IBOutlet weak var level: UISegmentedControl!
+    @IBOutlet weak var gpaSlider: UISlider!
+    @IBOutlet weak var experience: UISegmentedControl!
+    
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var output: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        output.text = "My name is \(nameField.text ?? "Salon"). I am in \(courseName[course.selectedSegmentIndex]) Level1 with the current GPA of \(gpaSlider.value) My background in iOS is \(experienceYears[experience.selectedSegmentIndex])"
+    }
+    
+    @IBAction func courseUpdate(_ sender: Any) {
+        
+        if(course.selectedSegmentIndex == 0){courseDisplayName = courseName[course.selectedSegmentIndex]}
+        else{courseDisplayName = courseName[course.selectedSegmentIndex]}
+        updateOuput()
+    }
+    
+    @IBAction func levelUpdate(_ sender: Any) {
+        
+        if(level.selectedSegmentIndex == 0){presentLevel = levelAs[level.selectedSegmentIndex]}
+        else if(level.selectedSegmentIndex == 1){presentLevel = levelAs[level.selectedSegmentIndex]}
+        else if(level.selectedSegmentIndex == 2){presentLevel = levelAs[level.selectedSegmentIndex]}
+        else if(level.selectedSegmentIndex == 3){presentLevel = levelAs[level.selectedSegmentIndex]}
+        else{presentLevel = levelAs[level.selectedSegmentIndex]}
+        updateOuput()
+    }
+    
+    @IBAction func experienceUpdate(_ sender: Any) {
+        
+        if(experience.selectedSegmentIndex==0){ experienceName = experienceYears[experience.selectedSegmentIndex]}
+        else if(experience.selectedSegmentIndex==1){ experienceName = experienceYears[experience.selectedSegmentIndex]}
+        else{ experienceName = experienceYears[experience.selectedSegmentIndex]}
+        updateOuput()
+    }
+    
+    @IBAction func sliderUpdate(_ sender: Any) {
+        updateOuput()
+    }
+    
+    func updateOuput(){
+        output.text = "My name is \(nameField.text ?? "Salon"). I am in \(courseName[course.selectedSegmentIndex]) Level1 with the current GPA of \(gpaSlider.value) My background in iOS is \(experienceYears[experience.selectedSegmentIndex])"
+    }
+    
+}
