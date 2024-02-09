bc068a4d15e6ef2e188dda7be67ee9e9250322c0
slama-pakhrin <salonlama47@gmail.com>
2024-02-09 14:27:42 -0500

    Second View Controller Update

---
+++ AssignmentOne/myTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  myTableViewCell.swift
+//  AssignmentOne
+//
+//  Created by Arii on 2024-02-09.
+//
+
+import UIKit
+
+class myTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
